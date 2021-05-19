2d9cbaba6956681ed2f65c5424fc5e237ef1a55e
Alina Elena Moroianu <alina.elena.moroianu@gmail.com>
2021-05-19 13:38:03 +0200

    Segundo commit de la rama cambios.

---
+++ actividad4/src/cambios/Cambios1.java
@@ -0,0 +1,10 @@
+package cambios;
+
+public class Cambios1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Segunda clase de cambio.");
+	}
+
+}
