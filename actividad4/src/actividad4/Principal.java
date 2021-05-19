c833d9c00c5423e608ddd823c99468f8adf70a2a
Alina Elena Moroianu <alina.elena.moroianu@gmail.com>
2021-05-19 13:17:00 +0200

    Segundo commit rehecho.

--- actividad4/src/actividad4/Principal.java
+++ actividad4/src/actividad4/Principal.java
@@ -7,6 +7,8 @@ public class Principal {
 		System.out.println("Hola mundo. Java Git.");
 		System.out.println("Hola mundo. Java Git2.");
 		System.out.println("Hola mundo. Java Git3.");
+		System.out.println("Hola mundo. Java Git4.");
+		//System.out.println("Hola mundo. Java Git5.");
 	}
 
 }
