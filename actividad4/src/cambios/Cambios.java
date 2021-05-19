167ced237d68dcd1b2b9e437c17fa686331760c1
Alina Elena Moroianu <alina.elena.moroianu@gmail.com>
2021-05-19 13:35:40 +0200

    Primer commit de la rama cambios.

---
+++ actividad4/src/cambios/Cambios.java
@@ -0,0 +1,10 @@
+package cambios;
+
+public class Cambios {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Primera clase de cambio.");
+	}
+
+}
