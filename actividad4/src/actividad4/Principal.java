82ebbfcf2ec91c0875f9c3a9b63ccf1e73cbef00
Alina Elena Moroianu <alina.elena.moroianu@gmail.com>
2021-05-19 13:11:58 +0200

    Segundo commit.

--- actividad4/src/actividad4/Principal.java
+++ actividad4/src/actividad4/Principal.java
@@ -5,7 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo. Java Git.");
-		//System.out.println("Hola mundo. Java Git1.");
+		System.out.println("Hola mundo. Java Git1.");
 		//System.out.println("Pruebas Git. Se añade cambio1.");
 	}
 
